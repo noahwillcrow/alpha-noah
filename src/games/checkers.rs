b61527ad521dae578dcb31c7529a7ff0a1411397
Noah Crowley <noahwillcrow@gmail.com>
2021-08-29 21:48:47 -0400

    further update comment

--- src/games/checkers.rs
+++ src/games/checkers.rs
@@ -9,6 +9,8 @@ use std::collections::HashSet;
 // This implementation is far less space-efficient as it will take up to 5 + (24*8) = 197 bits to store information,
 // which then is rounded up to the nearest byte for a total of 200 bits.
 // The length of the hash is proportional to the number of pieces on the board, so the average hash length is a complex thing to calculate.
+// Assuming the inaccurate number of 25 bytes per state, it would take rougly 2.5e12 GB to store every single hash possible to represent all 10^20 states.
+// This implementation assumes that that's not a feasible number of states to actually explore in one iteration of the program.
 // So exactly how does the hashing work here?
 // Each state hashes to a maximum of 25 bytes - a byte to represent how many pieces there are and one byte per piece up to 24 pieces.
 // Each piece is hashed to use the first two bits to represent its type:
