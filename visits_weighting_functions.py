474bbfcd0e7db825be04eb5d645c64e0e17e2b4c
Noah Crowley <noahwillcrow@gmail.com>
2021-08-29 21:49:30 -0400

    Goodbye Python

--- visits_weighting_functions.py
+++
@@ -1,8 +0,0 @@
-def zero(num_visits, min_available_visits, max_available_visits):
-    return 0
-
-def one(num_visits, min_available_visits, max_available_visits):
-    return 1
-
-def difference_from_max(num_visits, min_available_visits, max_available_visits):
-    return max_available_visits - num_visits
